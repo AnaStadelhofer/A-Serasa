57466ff7ea9a31841f176173b544e364f87a6137
Ana Stadelhofer <ana.stadelhofer@outlook.com>
2026-05-09 10:48:21 -0300

    Iniciando o projeto

---
+++ cypress.config.js
@@ -0,0 +1,15 @@
+const { defineConfig } = require("cypress");
+
+module.exports = defineConfig({
+  allowCypressEnv: false,
+
+  e2e: {
+    baseUrl: 'http://google.com',
+    specPattern: 'cypress/e2e/**/*.spec.cy.{js,ts}',
+    supportFile: 'cypress/support/e2e.js',
+    env: {
+      apiUrl: 'https://jsonplaceholder.typicode.com/',
+    },
+    setupNodeEvents(on, config) { },
+  },
+});
