0a8d86df5d7e688e49943ced027fc2da4a54dd53
Ana Stadelhofer <ana.stadelhofer@outlook.com>
2026-05-09 10:55:51 -0300

    Mapeamento de cenários do endpoint posts

--- cypress/e2e/api/posts.spec.cy.js
+++ cypress/e2e/api/posts.spec.cy.js
@@ -1,5 +1,27 @@
-describe('template spec', () => {
-  it('passes', () => {
-    cy.visit('https://example.cypress.io')
+describe("Testes do endpoint Posts", () => {
+
+  it("Dado que o usuário consulte todos os posts", () => {
+    cy.log("teste de posts");
   })
+
+  it("Dado que o usuário consulte um post específico", () => {
+    cy.log("teste de post específico");
+  })
+
+  it("Dado que o usuário crie um novo post", () => {
+    cy.log("teste de criação de post");
+  })
+
+  it("Dado que o usuário atualize todos dados um post existente", () => {
+    cy.log("teste de atualização de post");
+  })
+
+  it("Dado que o usuário atualize parcialmente um post existente", () => {
+    cy.log("teste de atualização parcial de post");
+  })
+
+  it("Dado que o usuário delete um post existente", () => {
+    cy.log("teste de deleção de post");
+  })
+
 })
